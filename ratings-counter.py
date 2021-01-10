adfd50fdce270e7e144c70fca863869f50fabfa0
Sarah <saraheunjikim@gmail.com>
2021-01-09 19:42:25 -0600

    Added files
    New learning

--- ratings-counter.py
+++ ratings-counter.py
@@ -2,7 +2,7 @@ from pyspark import SparkConf, SparkContext
 import collections
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
 lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
